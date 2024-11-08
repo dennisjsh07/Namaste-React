31111c03421b2c7e58c51370eb366367a7f89bf6
dennis <dennisjshofficial@gmail.com>
2024-11-08 08:03:09 +0530

    adding data to swiggy

---
+++ 04-Swiggy/app.js
@@ -0,0 +1,68 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png?industry=COMPANY+NAME"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const ResCard = (props) => {
+    const {resName, cuisine} = props
+  return (
+    <div
+      className="res-card"
+      style={{
+        backgroundColor: "#f0f0f0",
+      }}
+    >
+      <img
+        className="res-logo"
+        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/FOOD_CATALOG/IMAGES/CMS/2024/8/29/a92291cb-54eb-48d7-879a-17052b118377_5e4b5418-6f2e-4dd4-a0ff-b1f6ac0d2c6b.JPG"
+      />
+      <h3>{resName}</h3>
+      <h4>{cuisine}</h4>
+      <h4>4.4 stars</h4>
+      <h4>38 minutes</h4>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search-bar">Seacrh</div>
+      <div className="res-container">
+        <ResCard resName="Meghana Foods" cuisine="Biriyani, North Indian, Asian"/>
+        <ResCard resName="KFC" cuisine="Drinks and Fries"/>
+      </div>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout />);
